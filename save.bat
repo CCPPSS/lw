561c411ff668ebac3f7cb2c6936cbb9a868d4671
CPS <holy.dandelion@139.com>
2019-12-12 00:07:17 +0800

    [Fast Save] 2019/12/12 周四- 0:07:16.96

--- save.bat
+++ save.bat
@@ -1,3 +1,26 @@
+@REM @Author: CPS
+@REM @Date:   2019-12-11 23:42:17
+@REM @Last Modified by:   CPS
+@REM Modified time: 2019-12-12 00:06:13
+
+
+@echo off
+@for /f "delims=/ tokens=1-3" %%a in ("%date:~0,10%") do (call set "newDate=%%a%%b%%c")
+@for /f "tokens=* delims= " %%a in ("%time:~0,5%") do (for /f "tokens=1,2 delims=:" %%i in ("%%a") do (call set "newTime=%%i:%%j"))
+
 git add .
-git commit -m "fast save %DATE%"
+
+@if "%1%"=="" (goto save)
+set "ip=%1%"
+@set "saveName=[%ip%]-%newDate%-%newTime%"
+
+git commit -m %saveName%
 git push
+goto end
+
+:save
+git commit -m "[Fast Save] %DATE%-%time%"
+git push
+
+:end
+exit
