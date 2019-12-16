333488e2cedf3a2ca7f33879e754659d0a42994d
CPS <holy.dandelion@139.com>
2019-12-16 18:44:54 +0800

    [Fast Save] 2019/12/16 周一-18:44:54.71

--- save.bat
+++ save.bat
@@ -15,12 +15,12 @@ set "ip=%1%"
 @set "saveName=[%ip%]-%newDate%-%newTime%"
 
 git commit -m %saveName%
-git push
+git push -u save master -f
 goto end
 
 :save
 git commit -m "[Fast Save] %DATE%-%time%"
-git push
+git push -u save master -f
 
 :end
 exit
